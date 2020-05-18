cfa703e7ff5a506dbb524784d6a1375d4838e4c9
Baris Unluer <unluerbaris@gmail.com>
2020-05-18 17:27:01 +0900

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :projects, only: [:new, :create, :show]
+  resources :matches, only: [:create]
 end
