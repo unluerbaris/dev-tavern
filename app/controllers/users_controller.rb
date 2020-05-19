5a9f0127f7893a25fade02dfac73345f1e937220
Grant Holley <gvholley@gmail.com>
2020-05-19 18:07:40 +0900

    Added projects and username to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    if User.find_by_username(params[:id])
+      @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User not found!"
+    end
+    @projects = Project.where("user_id = ?" , User.find_by_username(params[:id]).id)
+    @newProject = Project.new
   end
 end
