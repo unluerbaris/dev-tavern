6544b3719416bcc8159845460342486ac6c4f359
Chihiro Tsuda <chihirot0109@gmail.com>
2020-05-20 21:03:56 +0900

    changed colors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :tagged]
 
   def home
     @projects = Project.last(10).reverse!
